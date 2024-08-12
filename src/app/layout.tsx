d347d8aad1e0b5e5861c0d8e318d14763e87bcea
Sebastian Kawelke <kawelkesebastian@gmail.com>
2024-08-12 15:17:29 +0200

    Adds google event data
    Signed-off-by: Sebastian Kawelke <kawelkesebastian@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,36 @@ export const metadata: Metadata = {
 
 const env = process.env.NODE_ENV
 
+const structuredData = {
+  '@context': 'https://schema.org',
+  '@type': 'Event',
+  name: 'Cybersecurity Hackathon 2024',
+  startDate: '2024-11-08T17:00',
+  endDate: '2024-11-10T23:00',
+  eventAttendanceMode: 'https://schema.org/OfflineEventAttendanceMode',
+  eventStatus: 'https://schema.org/EventScheduled',
+  location: {
+    '@type': 'Place',
+    name: 'DIGITALHUB.DE',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Am Hauptbahnhof 6',
+      addressLocality: 'Bonn',
+      postalCode: '53111',
+      addressRegion: 'NRW',
+      addressCountry: 'DE',
+    },
+  },
+  image: ['https://cybersecurity-hackathon.de/teilnehmende-2023.jpg'],
+  description:
+    'Nimm am Cybersecurity Hackathon 2024 in Bonn vom 8. - 10. November teil! Entwickle Lösungen für OSS Sicherheit, vernetze dich mit Experten und gewinne 3k € Preisgeld. Kostenlose Teilnahme, spannende Workshops und eine großartige Atmosphäre erwarten dich.',
+  organizer: {
+    '@type': 'Organization',
+    name: 'L3montree Cybersecurity',
+    url: 'https://l3montree.com',
+  },
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -52,6 +82,10 @@ export default function RootLayout({
           ></script>
         )}
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        ></script>
       </head>
       <body className="flex min-h-full">
         <div className="flex w-full flex-col">{children}</div>
