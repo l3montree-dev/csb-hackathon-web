cae2d6b0ff153a22af24d8ae6474b38313ebc649
Frederic Noppe <frederic.noppe@l3montree.com>
2024-11-05 11:50:49 +0100

    Updates Buttons
    Signed-off-by: Frederic Noppe <frederic.noppe@l3montree.com>

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -290,7 +290,7 @@ function TimeSlots({ day, className }: { day: Day; className?: string }) {
             <p className="mt-1 text-sm font-medium tracking-tight text-l3-400">
               Offen mit{' '}
               <a
-                href="/coming-soon"
+                href="https://pretix.eu/l3montree/cybers-hack-2024/"
                 className="text-l3-400 underline decoration-dashed decoration-1 underline-offset-2 hover:text-l3-200"
               >
                 Besucherticket
